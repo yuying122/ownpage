196e10daa153d864b6b2f1e7329563baa35d0626
yuying122 <yunming1005@gmail.com>
2023-11-13 03:25:35 +0800

    v0

---
+++ index.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    homepage = "<h1>資管二B 411146287 施艾岑</h1>"
+    homepage += "<a href=/homepage>個人網站</a><br>"
+    return homepage
+
+
+@app.route("/homepage")
+def homepage():
+    return render_template("homepage.html")
+
+
+
+#if __name__ == "__main__":
+#    app.run()
