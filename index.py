45936a757ebda6913394f4ce5bdaac3b97f708e9
yuying122 <yunming1005@gmail.com>
2023-11-13 03:31:10 +0800

    v0

--- index.py
+++ index.py
@@ -4,14 +4,28 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     homepage = "<h1>資管二B 411146287 施艾岑</h1>"
-    homepage += "<a href=/homepage>個人網站</a><br>"
+    homepage += "<a href=/me>自傳履歷</a><br>"
+    homepage += "<a href=/hollandcode>興趣何倫碼測驗結果</a><br>"
+    homepage += "<a href=/work>MIS相關工作</a><br>"
+    homepage += "<a href=/webpage>查看連結</a><br>"
     return homepage
 
 
-@app.route("/homepage")
+@app.route("/me")
 def homepage():
-    return render_template("homepage.html")
+    return render_template("me.html")
 
+@app.route("/hollandcode")
+def homepage():
+    return render_template("hollandcode.html")  
+    
+@app.route("/work")
+def homepage():
+    return render_template("work.html")
+
+@app.route("/webpage")
+def homepage():
+    return render_template("webpage.html")          
 
 
 #if __name__ == "__main__":
